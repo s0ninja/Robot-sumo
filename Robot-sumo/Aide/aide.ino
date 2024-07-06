8264e7068c9063812d7d358cdc0eaf50197c097f
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-07-06 18:59:35 +0200

    Add files via upload

---
+++ Robot-sumo/Aide/aide.ino
@@ -0,0 +1,51 @@
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌ ▀▀▀▀▀█░█▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌          ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌     ▐░▌     ▐░▌▐░▌    ▐░▌      ▐░▌    ▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌     ▐░▌     ▐░▌ ▐░▌   ▐░▌      ▐░▌    ▐░█▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌     ▐░▌     ▐░▌  ▐░▌  ▐░▌      ▐░▌    ▐░░░░░░░░░░░▌
+ ▀▀▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌     ▐░▌     ▐░▌   ▐░▌ ▐░▌      ▐░▌    ▐░█▀▀▀▀▀▀▀█░▌
+          ▐░▌▐░▌       ▐░▌▐░▌    ▐░▌▐░▌     ▐░▌     ▐░▌    ▐░▌▐░▌      ▐░▌    ▐░▌       ▐░▌
+ ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌ ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌ ▄▄▄▄▄█░▌    ▐░▌       ▐░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░▌    ▐░▌       ▐░▌
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀      ▀         ▀ 
+*/
+
+
+
+/*------------------------------------------------------------------------------------------------
+
+Les codes se trouvant dans le dossier || Robot-simo > code || doivent être adaptés à vos robots 
+en raison des défaillances multiples des roues, de leur sens ainsi que des capteurs
+
+------------------------------------------------------------------------------------------------*/
+
+#define mon_nom 5 //ça sert a définir la broche "mon_nom" sur la broche 5
+
+digitalWrite( , ); //Faire l'information avec une information 0 ou  1
+analogWrite( , ); //Faire l'information entre une information 0 à 255
+
+digitalRead( , ); //Lire une information 
+
+//---------------------------------- CONDITION ------------------------------------------------------
+
+if(){} //Si
+else(){} //Sinon
+
+else if () {} // Sinon si, il faut l'use seulement si on a d'autre condition
+
+/*
+ En résumé, ce sont des conditions, comme leur nom l'indique. Toutefois, il faut noter que "if" et "else" ne peuvent 
+ être utilisés qu'une seule fois chacun, contrairement à "else if" qui peut être utilisé plusieurs fois.
+*/
+
+//--------------------------------------------- FONCTION ---------------------------------------------
+
+void setup {} //sert a setup (à mettre en place) le programme | Donner les information de base à ton programme
+void loop {} //On vas éxécuté le programme ici
+
+//--------------------------------------------- ATTENTION ---------------------------------------------
+
+/*Les codes se trouvant dans le dossier || Robot-sumo > code || doivent être adaptés à vos robots 
+en raison des défaillances multiples des roues, de leur sens ainsi que des capteurs*/
